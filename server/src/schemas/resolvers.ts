bb1ff0d138850173171fe6cd5f3c64f4af3facd4
ant-codes-42 <schwab.it@pm.me>
2025-02-26 20:57:51 -0600

    added boilerplate configuration

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,7 @@
+import { User } from '../models/index.js';
+
+const resolvers = {
+
+};
+
+export default resolvers;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { Request, Response } from 'express';
+
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+import { typeDefs, resolvers } from './schemas/index.js';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+await server.start();
+await db();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,10 +22,15 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
-app.use(routes);
+app.use('/graphql', expressMiddleware(server));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 });
